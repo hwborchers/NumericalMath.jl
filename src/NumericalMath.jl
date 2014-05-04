d61def7762fa2cf0cd40ac6b06234b0bcebbcd9e
hwborchers <hwborchers@googlemail.com>
2014-05-04 15:50:09 +0200

    Update NumericalMath.jl

--- src/NumericalMath.jl
+++ src/NumericalMath.jl
@@ -3,7 +3,12 @@ module NumericalMath
 # package code goes here
 
   export ridders, bisect, brent_dekker
+  export trapz
+  export lambert_W
 
   include("fzero.jl")
+  include("integration.jl")
+  include("special.jl")
+
 
 end # module
