126450422ecd5e050b7a24cd9a6a5424b955e303
hwborchers <hwborchers@googlemail.com>
2014-05-04 15:48:49 +0200

    Create integration.jl

---
+++ src/integration.jl
@@ -0,0 +1,25 @@
+##
+##  i n t e g r a t i o n . j l  Integration Routines
+##
+
+
+function trapz{Tx<:Number, Ty<:Number}(x::Vector{Tx}, y::Vector{Ty})
+    # Trapezoidal integration rule
+    local n = length(x)
+    if (length(y) != n)
+        error("Vectors 'x', 'y' must be of same length")
+    end
+    r = zero(zero(Tx) + zero(Ty))
+    if n == 1; return r; end
+    for i in 2:n
+        r += (x[i] - x[i-1]) * (y[i] + y[i-1])
+    end
+    #= correction -h^2/12 * (f'(b) - f'(a))
+    ha = x[2] - x[1]
+    he = x[end] - x[end-1]
+    ra = (y[2] - y[1]) / ha
+    re = (y[end] - y[end-1]) / he
+    r/2 - ha*he/12 * (re - ra)
+    =#
+    return r/2
+end
