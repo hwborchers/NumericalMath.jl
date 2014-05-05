ce517a27d2c1ef05494537dfa50b5365bd890463
hwborchers <hwborchers@googlemail.com>
2014-05-05 07:28:26 +0200

    Update NumericalMath.jl

--- src/NumericalMath.jl
+++ src/NumericalMath.jl
@@ -2,15 +2,14 @@ module NumericalMath
 
 # package code goes here
 
-  export pval, horner
-  export ridders, bisect, brent_dekker
-  export trapz
-  export lambert_W
+  export pval, horner,
+         ridders, bisect, brent_dekker,
+         trapz,
+         lambert_W
 
   include("polynomial.jl")
   include("fzero.jl")
   include("integration.jl")
   include("special.jl")
-
 
 end # module
