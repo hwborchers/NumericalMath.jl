17198cca289c3e88570f356233012857e617fdff
hwborchers <hwborchers@googlemail.com>
2014-05-10 16:02:34 +0200

    Update polynomial.jl

--- src/polynomial.jl
+++ src/polynomial.jl
@@ -3,28 +3,12 @@
 ##
 
 
-function pval(p::Vector, x::Number)
+function horner{T<:Number}(p::Vector{T}, x::T)
     local n = length(p)
     if n == 0
-        return NaN
+        return 0,0
     elseif n == 1
-        return p[1]
-    else
-        y = p[1]
-        for i in 2:n
-            y = y * x + p[i]
-        end
-    end
-    return y
-end
-
-
-function horner(p::Vector, x::Number)
-    local n = length(p)
-    if n == 0
-        return NaN, NaN
-    elseif n == 1
-        return p[1], 0
+        return p[1],0
     else
         y = p[1]; dy = 0
         for i in 2:n
@@ -32,5 +16,22 @@ function horner(p::Vector, x::Number)
             y  =  y * x + p[i]
         end
     end
-    return y, dy
+    return y,dy
+end
+
+
+function horner{T<:Number}(p::Vector{T}, x::Vector{T})
+    local n = length(p)
+    if n == 0
+        return [0],[0]
+    elseif n == 1
+        return p,[0]
+    else
+        y = p[1]; dy = 0
+        for i in 2:n
+            dy = dy .* x .+ y
+            y  =  y .* x .+ p[i]
+        end
+    end
+    return y,dy
 end
