521a4c175d074d2aa9f70fb9edb7b3d191d15963
hwborchers <hwborchers@googlemail.com>
2014-05-27 18:11:20 +0200

    Update special.jl

--- src/special.jl
+++ src/special.jl
@@ -6,7 +6,7 @@
 # Lambert W function, inverse of x -> x exp(x)
 # its derivative is 1 / (1+zw) / exp(zw) where zw = lambert_W(z)
 #
-function lambert_W(x::Real)
+function lambertW(x::Real)
     if x < -1/exp(1); return NaN; end
     w0 = 1.0
     w1 = w0 - (w0 * exp(w0) - x)/((w0 + 1) * exp(w0) - 
