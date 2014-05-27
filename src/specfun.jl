9df6df7230a53f62c0e491eeebd8a73adbd1a3b3
hwborchers <hwborchers@googlemail.com>
2014-05-27 20:23:26 +0200

    Update specfun.jl

--- src/specfun.jl
+++ src/specfun.jl
@@ -4,17 +4,23 @@
 
 
 # Lambert W function, inverse of x -> x exp(x)
-# its derivative is 1 / (1+zw) / exp(zw) where zw = lambert_W(z)
+# its derivative is 1 / (1+zw) / exp(zw) where zw = lambertW(z)
 #
 function lambertW(x::Real)
-    if x < -1/exp(1); return NaN; end
-    w0 = 1.0
-    w1 = w0 - (w0 * exp(w0) - x)/((w0 + 1) * exp(w0) - 
-        (w0 + 2) * (w0 * exp(w0) - x)/(2 * w0 + 2))
+    if x <  -1.0/exp(1.0); return NaN, NaN;  end
+    if x == -1.0/exp(1.0); return -Inf, Inf; end
+
+    local w0::Real = 1.0, w1::Realw2::Real
+    w1 = w0 - (w0 * exp(w0) - x)/((w0 + 1.0) * exp(w0) - 
+        (w0 + 2.0) * (w0 * exp(w0) - x)/(2.0 * w0 + 2.0))
+
     while abs(w1 - w0) > 1e-15
         w0 = w1
-        w1 = w0 - (w0 * exp(w0) - x)/((w0 + 1) * exp(w0) - 
-            (w0 + 2) * (w0 * exp(w0) - x)/(2 * w0 + 2))
+        w1 = w0 - (w0 * exp(w0) - x)/((w0 + 1.0) * exp(w0) - 
+            (w0 + 2.0) * (w0 * exp(w0) - x)/(2.0 * w0 + 2.0))
     end
-    return w1
+
+    w2 = 1.0 / (1 + w1) / exp(w1)
+
+    return w1, w2
 end
