6cacf1711a2031b502f6117e44638011c8a8e8e1
hwborchers <hwborchers@googlemail.com>
2014-05-27 18:07:48 +0200

    Update NumericalMath.jl

--- src/NumericalMath.jl
+++ src/NumericalMath.jl
@@ -2,14 +2,23 @@ module NumericalMath
 
 # package code goes here
 
-  export horner,
-         ridders, bisect, brent_dekker,
-         trapz,
-         lambert_W
-
+  export trisolve
+  export pval, horner, pzero
+  export interp1d, pchip
+  export ridders, brent_dekker
+  export fd_gradient, fd_jacobian, fd_hessian, fd_laplacian,
+         numderiv, complex_step
+  export trapz, romberg, line_integral
+  export agm, arc_length
+  export lambertW
+  
+  include("la.jl")
   include("polynomial.jl")
+  include("interpolate.jl")
   include("fzero.jl")
-  include("integration.jl")
-  include("special.jl")
+  include("derivative.jl")
+  include("integrate.jl")
+  include("misc.jl")
+  include("specfun.jl")
 
 end # module
